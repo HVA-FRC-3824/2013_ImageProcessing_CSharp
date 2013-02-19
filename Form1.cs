4914e54394c1616358e742b16079fa65859705c2
Owen Woods <ih8aiftp>
2013-02-18 20:11:31 -0500

    Merged Frisbee IP and Target IP.
    IP: Image Processing code

--- Form1.cs
+++ Form1.cs
@@ -50,6 +50,23 @@ namespace ImageProcessing
 
         private void processImage(object sender, EventArgs arg)
         {
+            string target_type = NetworkTable.getTable("SmartDashboard").getString("Target Type");
+            if (target_type == "frisbee")
+            {
+                processImageFrisbee();
+            } 
+            else if (target_type == "target")
+            {
+                processImageTarget();
+            } 
+            else 
+            {
+        //print error
+            }
+        }
+
+        private void processImageFrisbee()
+        {
  
             Image<Bgr, Byte> image = capture.QueryFrame();
 
@@ -180,6 +197,103 @@ namespace ImageProcessing
             
 
             
+        }
+
+        private void processImageTarget()
+        {
+
+            Image<Bgr, Byte> image = capture.QueryFrame();
+
+
+            Image<Hsv, byte> hsvImage;
+            Image<Gray, byte> mask;
+
+
+            //image = new Image<Bgr, Byte>("C:\\Users\\Administrator\\Documents\\Visual Studio 2010\\Projects\\ImageProcessing2013CSharp\\retroreflective.png");
+            //image = new Image<Bgr, Byte>("C:\\Users\\Administrator\\Documents\\Visual Studio 2010\\Projects\\ImageProcessing2013CSharp\\Robot 3 - 26ft.png");
+
+            // The SmoothGaussian makes the image much MUCH smoother.
+            //image.SmoothGaussian(299, 299, 250.0, 250.0);
+            // Convert the image to hsv
+            hsvImage = image.Convert<Hsv, byte>();
+
+            //mask = hsvImage.InRange(new Hsv(130.0 / 2, 25.0, 30.0), new Hsv(150.0 / 2, 255.0, 255.0));
+            //mask = hsvImage.InRange(new Hsv(hMin, sMin, vMin), new Hsv(hMax, sMax, vMax));
+            mask = hsvImage.InRange(new Hsv((int)(103 / 240.0f * 180),
+                        (int)(100 / 240.0f * 255),
+                        (int)(150 / 240.0f * 255)),
+                    new Hsv((int)(118 / 240.0f * 180),
+                        (int)(240 / 240.0f * 255),
+                        (int)(240 / 240.0f * 255)));
+            //Hsv hsv = new Hsv(
+
+            // Filter image
+            //mask._Erode(4);
+            mask._Dilate(4);
+            mask._Erode(4);
+
+
+
+            //      mask._SmoothGaussian(image, 2, 2, 1.5, 1.5);
+
+            // Find the Rectangles
+            //contours = mask.FindContours(Emgu.CV.CvEnum.CHAIN_APPROX_METHOD.CV_CHAIN_APPROX_SIMPLE, Emgu.CV.CvEnum.RETR_TYPE.CV_RETR_TREE);
+
+            // List of boxes found
+            List<Rectangle> boxList = new List<Rectangle>();
+
+            for (Contour<Point> contours = mask.FindContours(); contours != null; contours = contours.HNext)
+            {
+                contours.ApproxPoly(contours.Perimeter * 0.05);
+                boxList.Add(contours.BoundingRectangle);
+            }
+
+            const double imageWidth = 320;
+            double minCenter = imageWidth; //min distance to center of image
+            int centerRectNum = 0;  //number of closest rect
+
+            for (int n = 0; n < boxList.Count; n++)
+            {
+                Rectangle rec = boxList[n];
+                image.Draw(rec, new Bgr(255, 21, 255), 2);
+                double centerx = rec.X + rec.Width / 2.0;
+                if (Math.Abs(centerx - imageWidth / 2) < minCenter)
+                {
+                    minCenter = Math.Abs(centerx - imageWidth / 2);
+                    centerRectNum = n;
+                }
+            }
+
+            //Console.Write(minCenter);
+            //MessageBox.Show("Pos:" + (boxList[centerRectNum].X + boxList[centerRectNum].Width/2.0)+"distance: "+minCenter);
+            //MessageBox.Show("Off by:" + (boxList[centerRectNum].X + boxList[centerRectNum].Width / 2.0 - imageWidth/2));
+
+
+            // Print out the information about the first rect
+            if (boxList.Count != 0)
+            {
+                double offset = (boxList[centerRectNum].X + boxList[centerRectNum].Width / 2.0 - imageWidth / 2);
+                //SetText("Off by:" + offset);
+                NetworkTable.getTable("SmartDashboard").putNumber("camera offset", offset);
+                //NetworkTable.getTable("SmartDashboard").putNumber("x", boxList[0].X);
+                //NetworkTable.getTable("SmartDashboard").putNumber("y", boxList[0].Y);
+                //NetworkTable.getTable("SmartDashboard").putNumber("h", boxList[0].Height);
+                //NetworkTable.getTable("SmartDashboard").putNumber("w", boxList[0].Width);
+                //NetworkTable.getTable("SmartDashboard").putNumber("servo", boxList[0].X / 320.0);
+            }
+            else
+            {
+                NetworkTable.getTable("SmartDashboard").putNumber("camera offset", 0);
+            }
+
+            //table.putNumber("index", i);
+            im_mask.Image = mask.ToBitmap();
+
+            im_image.Image = image.ToBitmap();
+            //SetImage(image.ToBitmap());
+
+
+
         }
 
         private void btn_start_Click(object sender, EventArgs e)
