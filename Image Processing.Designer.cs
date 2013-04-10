bcc400f8dbe0430170234268e2ea1582aed288ce
Mike Goin <mike.goin12@gmail.com>
2013-04-09 20:32:52 -0400

    Changes at the competition and changes after by John

---
+++ Image Processing.Designer.cs
@@ -0,0 +1,335 @@
+﻿namespace ImageProcessing
+{
+    partial class ImageProcessing
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bar_hMin = new System.Windows.Forms.TrackBar();
+            this.lb_hMin = new System.Windows.Forms.Label();
+            this.lb_vMin = new System.Windows.Forms.Label();
+            this.bar_vMin = new System.Windows.Forms.TrackBar();
+            this.lb_sMin = new System.Windows.Forms.Label();
+            this.bar_sMin = new System.Windows.Forms.TrackBar();
+            this.bar_vMax = new System.Windows.Forms.TrackBar();
+            this.lb_sMax = new System.Windows.Forms.Label();
+            this.bar_sMax = new System.Windows.Forms.TrackBar();
+            this.lb_vMax = new System.Windows.Forms.Label();
+            this.lb_hMax = new System.Windows.Forms.Label();
+            this.bar_hMax = new System.Windows.Forms.TrackBar();
+            this.im_mask = new System.Windows.Forms.PictureBox();
+            this.im_image = new System.Windows.Forms.PictureBox();
+            this.lb_hMinValue = new System.Windows.Forms.Label();
+            this.lb_sMinValue = new System.Windows.Forms.Label();
+            this.lb_vMinValue = new System.Windows.Forms.Label();
+            this.lb_hMaxValue = new System.Windows.Forms.Label();
+            this.lb_sMaxValue = new System.Windows.Forms.Label();
+            this.lb_vMaxValue = new System.Windows.Forms.Label();
+            this.tb_message = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.bar_hMin)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bar_vMin)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bar_sMin)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bar_vMax)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bar_sMax)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bar_hMax)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.im_mask)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.im_image)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bar_hMin
+            // 
+            this.bar_hMin.Location = new System.Drawing.Point(52, 12);
+            this.bar_hMin.Maximum = 180;
+            this.bar_hMin.Name = "bar_hMin";
+            this.bar_hMin.Size = new System.Drawing.Size(220, 45);
+            this.bar_hMin.TabIndex = 1;
+            this.bar_hMin.Visible = false;
+            this.bar_hMin.Scroll += new System.EventHandler(this.bar_Scroll);
+            // 
+            // lb_hMin
+            // 
+            this.lb_hMin.AutoSize = true;
+            this.lb_hMin.Location = new System.Drawing.Point(12, 12);
+            this.lb_hMin.Name = "lb_hMin";
+            this.lb_hMin.Size = new System.Drawing.Size(34, 13);
+            this.lb_hMin.TabIndex = 2;
+            this.lb_hMin.Text = "H min";
+            this.lb_hMin.Visible = false;
+            // 
+            // lb_vMin
+            // 
+            this.lb_vMin.AutoSize = true;
+            this.lb_vMin.Location = new System.Drawing.Point(12, 73);
+            this.lb_vMin.Name = "lb_vMin";
+            this.lb_vMin.Size = new System.Drawing.Size(33, 13);
+            this.lb_vMin.TabIndex = 4;
+            this.lb_vMin.Text = "V min";
+            this.lb_vMin.Visible = false;
+            // 
+            // bar_vMin
+            // 
+            this.bar_vMin.Location = new System.Drawing.Point(52, 73);
+            this.bar_vMin.Maximum = 255;
+            this.bar_vMin.Name = "bar_vMin";
+            this.bar_vMin.Size = new System.Drawing.Size(220, 45);
+            this.bar_vMin.TabIndex = 3;
+            this.bar_vMin.Visible = false;
+            this.bar_vMin.Scroll += new System.EventHandler(this.bar_Scroll);
+            // 
+            // lb_sMin
+            // 
+            this.lb_sMin.AutoSize = true;
+            this.lb_sMin.Location = new System.Drawing.Point(12, 43);
+            this.lb_sMin.Name = "lb_sMin";
+            this.lb_sMin.Size = new System.Drawing.Size(33, 13);
+            this.lb_sMin.TabIndex = 6;
+            this.lb_sMin.Text = "S min";
+            this.lb_sMin.Visible = false;
+            // 
+            // bar_sMin
+            // 
+            this.bar_sMin.Location = new System.Drawing.Point(52, 43);
+            this.bar_sMin.Maximum = 255;
+            this.bar_sMin.Name = "bar_sMin";
+            this.bar_sMin.Size = new System.Drawing.Size(220, 45);
+            this.bar_sMin.TabIndex = 5;
+            this.bar_sMin.Visible = false;
+            this.bar_sMin.Scroll += new System.EventHandler(this.bar_Scroll);
+            // 
+            // bar_vMax
+            // 
+            this.bar_vMax.Location = new System.Drawing.Point(52, 185);
+            this.bar_vMax.Maximum = 255;
+            this.bar_vMax.Name = "bar_vMax";
+            this.bar_vMax.Size = new System.Drawing.Size(220, 45);
+            this.bar_vMax.TabIndex = 9;
+            this.bar_vMax.Visible = false;
+            this.bar_vMax.Scroll += new System.EventHandler(this.bar_Scroll);
+            // 
+            // lb_sMax
+            // 
+            this.lb_sMax.AutoSize = true;
+            this.lb_sMax.Location = new System.Drawing.Point(12, 155);
+            this.lb_sMax.Name = "lb_sMax";
+            this.lb_sMax.Size = new System.Drawing.Size(36, 13);
+            this.lb_sMax.TabIndex = 12;
+            this.lb_sMax.Text = "S max";
+            this.lb_sMax.Visible = false;
+            // 
+            // bar_sMax
+            // 
+            this.bar_sMax.Location = new System.Drawing.Point(52, 155);
+            this.bar_sMax.Maximum = 255;
+            this.bar_sMax.Name = "bar_sMax";
+            this.bar_sMax.Size = new System.Drawing.Size(220, 45);
+            this.bar_sMax.TabIndex = 11;
+            this.bar_sMax.Visible = false;
+            this.bar_sMax.Scroll += new System.EventHandler(this.bar_Scroll);
+            // 
+            // lb_vMax
+            // 
+            this.lb_vMax.AutoSize = true;
+            this.lb_vMax.Location = new System.Drawing.Point(12, 185);
+            this.lb_vMax.Name = "lb_vMax";
+            this.lb_vMax.Size = new System.Drawing.Size(36, 13);
+            this.lb_vMax.TabIndex = 10;
+            this.lb_vMax.Text = "V max";
+            this.lb_vMax.Visible = false;
+            // 
+            // lb_hMax
+            // 
+            this.lb_hMax.AutoSize = true;
+            this.lb_hMax.Location = new System.Drawing.Point(12, 124);
+            this.lb_hMax.Name = "lb_hMax";
+            this.lb_hMax.Size = new System.Drawing.Size(37, 13);
+            this.lb_hMax.TabIndex = 8;
+            this.lb_hMax.Text = "H max";
+            this.lb_hMax.Visible = false;
+            // 
+            // bar_hMax
+            // 
+            this.bar_hMax.Location = new System.Drawing.Point(52, 124);
+            this.bar_hMax.Maximum = 180;
+            this.bar_hMax.Name = "bar_hMax";
+            this.bar_hMax.Size = new System.Drawing.Size(220, 45);
+            this.bar_hMax.TabIndex = 7;
+            this.bar_hMax.Visible = false;
+            this.bar_hMax.Scroll += new System.EventHandler(this.bar_Scroll);
+            // 
+            // im_mask
+            // 
+            this.im_mask.Location = new System.Drawing.Point(591, 258);
+            this.im_mask.Name = "im_mask";
+            this.im_mask.Size = new System.Drawing.Size(320, 240);
+            this.im_mask.TabIndex = 13;
+            this.im_mask.TabStop = false;
+            // 
+            // im_image
+            // 
+            this.im_image.Location = new System.Drawing.Point(591, 12);
+            this.im_image.Name = "im_image";
+            this.im_image.Size = new System.Drawing.Size(320, 240);
+            this.im_image.TabIndex = 14;
+            this.im_image.TabStop = false;
+            // 
+            // lb_hMinValue
+            // 
+            this.lb_hMinValue.AutoSize = true;
+            this.lb_hMinValue.Location = new System.Drawing.Point(279, 13);
+            this.lb_hMinValue.Name = "lb_hMinValue";
+            this.lb_hMinValue.Size = new System.Drawing.Size(13, 13);
+            this.lb_hMinValue.TabIndex = 15;
+            this.lb_hMinValue.Text = "0";
+            this.lb_hMinValue.Visible = false;
+            // 
+            // lb_sMinValue
+            // 
+            this.lb_sMinValue.AutoSize = true;
+            this.lb_sMinValue.Location = new System.Drawing.Point(279, 43);
+            this.lb_sMinValue.Name = "lb_sMinValue";
+            this.lb_sMinValue.Size = new System.Drawing.Size(13, 13);
+            this.lb_sMinValue.TabIndex = 16;
+            this.lb_sMinValue.Text = "0";
+            this.lb_sMinValue.Visible = false;
+            // 
+            // lb_vMinValue
+            // 
+            this.lb_vMinValue.AutoSize = true;
+            this.lb_vMinValue.Location = new System.Drawing.Point(279, 73);
+            this.lb_vMinValue.Name = "lb_vMinValue";
+            this.lb_vMinValue.Size = new System.Drawing.Size(13, 13);
+            this.lb_vMinValue.TabIndex = 17;
+            this.lb_vMinValue.Text = "0";
+            this.lb_vMinValue.Visible = false;
+            // 
+            // lb_hMaxValue
+            // 
+            this.lb_hMaxValue.AutoSize = true;
+            this.lb_hMaxValue.Location = new System.Drawing.Point(279, 124);
+            this.lb_hMaxValue.Name = "lb_hMaxValue";
+            this.lb_hMaxValue.Size = new System.Drawing.Size(13, 13);
+            this.lb_hMaxValue.TabIndex = 18;
+            this.lb_hMaxValue.Text = "0";
+            this.lb_hMaxValue.Visible = false;
+            // 
+            // lb_sMaxValue
+            // 
+            this.lb_sMaxValue.AutoSize = true;
+            this.lb_sMaxValue.Location = new System.Drawing.Point(279, 155);
+            this.lb_sMaxValue.Name = "lb_sMaxValue";
+            this.lb_sMaxValue.Size = new System.Drawing.Size(13, 13);
+            this.lb_sMaxValue.TabIndex = 19;
+            this.lb_sMaxValue.Text = "0";
+            this.lb_sMaxValue.Visible = false;
+            // 
+            // lb_vMaxValue
+            // 
+            this.lb_vMaxValue.AutoSize = true;
+            this.lb_vMaxValue.Location = new System.Drawing.Point(279, 185);
+            this.lb_vMaxValue.Name = "lb_vMaxValue";
+            this.lb_vMaxValue.Size = new System.Drawing.Size(13, 13);
+            this.lb_vMaxValue.TabIndex = 20;
+            this.lb_vMaxValue.Text = "0";
+            this.lb_vMaxValue.Visible = false;
+            // 
+            // tb_message
+            // 
+            this.tb_message.Enabled = false;
+            this.tb_message.Location = new System.Drawing.Point(15, 222);
+            this.tb_message.Multiline = true;
+            this.tb_message.Name = "tb_message";
+            this.tb_message.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.tb_message.Size = new System.Drawing.Size(570, 276);
+            this.tb_message.TabIndex = 21;
+            // 
+            // ImageProcessing
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(923, 502);
+            this.Controls.Add(this.tb_message);
+            this.Controls.Add(this.lb_vMaxValue);
+            this.Controls.Add(this.lb_sMaxValue);
+            this.Controls.Add(this.lb_hMaxValue);
+            this.Controls.Add(this.lb_vMinValue);
+            this.Controls.Add(this.lb_sMinValue);
+            this.Controls.Add(this.lb_hMinValue);
+            this.Controls.Add(this.im_image);
+            this.Controls.Add(this.im_mask);
+            this.Controls.Add(this.bar_vMax);
+            this.Controls.Add(this.lb_sMax);
+            this.Controls.Add(this.bar_sMax);
+            this.Controls.Add(this.lb_vMax);
+            this.Controls.Add(this.lb_hMax);
+            this.Controls.Add(this.bar_hMax);
+            this.Controls.Add(this.bar_vMin);
+            this.Controls.Add(this.lb_sMin);
+            this.Controls.Add(this.bar_sMin);
+            this.Controls.Add(this.lb_vMin);
+            this.Controls.Add(this.lb_hMin);
+            this.Controls.Add(this.bar_hMin);
+            this.Name = "ImageProcessing";
+            this.Text = "Image Processing";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.ImageProcessing_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.bar_hMin)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bar_vMin)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bar_sMin)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bar_vMax)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bar_sMax)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bar_hMax)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.im_mask)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.im_image)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TrackBar bar_hMin;
+        private System.Windows.Forms.Label lb_hMin;
+        private System.Windows.Forms.Label lb_vMin;
+        private System.Windows.Forms.TrackBar bar_vMin;
+        private System.Windows.Forms.Label lb_sMin;
+        private System.Windows.Forms.TrackBar bar_sMin;
+        private System.Windows.Forms.TrackBar bar_vMax;
+        private System.Windows.Forms.Label lb_sMax;
+        private System.Windows.Forms.TrackBar bar_sMax;
+        private System.Windows.Forms.Label lb_vMax;
+        private System.Windows.Forms.Label lb_hMax;
+        private System.Windows.Forms.TrackBar bar_hMax;
+        private System.Windows.Forms.PictureBox im_mask;
+        private System.Windows.Forms.PictureBox im_image;
+        private System.Windows.Forms.Label lb_hMinValue;
+        private System.Windows.Forms.Label lb_sMinValue;
+        private System.Windows.Forms.Label lb_vMinValue;
+        private System.Windows.Forms.Label lb_hMaxValue;
+        private System.Windows.Forms.Label lb_sMaxValue;
+        private System.Windows.Forms.Label lb_vMaxValue;
+        private System.Windows.Forms.TextBox tb_message;
+    }
+}
+
---
+++ Image Processing.cs
@@ -0,0 +1,323 @@
+﻿#region Using Statements
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using edu.wpi.first.wpilibj.networktables;
+using edu.wpi.first.wpilibj.networktables2;
+using edu.wpi.first.wpilibj.tables;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.Util;
+using Emgu.CV.UI;
+using System.Threading;
+#endregion
+
+namespace ImageProcessing
+{
+    public partial class ImageProcessing : Form
+    {
+        #region *** Declaracations
+        #region Constants
+        private const int H_MIN = 0;
+        private const int H_MAX = 180;
+        private const int S_MIN = 0;
+        private const int S_MAX = 22;
+        private const int V_MIN = 172;
+        private const int V_MAX = 255;
+        #endregion
+
+        #region delegates
+        // delegate to support writing to the Windows text box through another thread
+        public delegate void WriteMessageDelegate(string text, bool linefeed);
+
+        public delegate int readTrackBarDelegate(TrackBar bar);
+        #endregion
+
+        #region Variables
+        private WriteMessageDelegate m_writeMessageToTextBox_Delegate;
+        private NetworkTable table;
+        private Capture capture;
+        #endregion
+        #endregion
+
+        #region Constructor
+        public ImageProcessing()
+        {
+            // inistialzie the dialog windows components
+            InitializeComponent();
+
+            m_writeMessageToTextBox_Delegate = writeMessageToTextBox;
+
+            // setup access to the network table
+            NetworkTable.setClientMode();
+            NetworkTable.setIPAddress("10.38.24.2");
+            table = NetworkTable.getTable("SmartDashboard");
+
+            // setup a link to the camera videl
+            //capture = new Capture("rtsp://192.168.0.90:554/axis-media/media.amp");
+            capture = new Capture("rtsp://10.38.24.11:554/axis-media/media.amp");
+
+            // This is for testing via webcam image.
+            //capture = new Capture();
+
+#if USE_SLIDERS
+            // show the controls
+            lb_hMin.Visible = true;
+            lb_hMax.Visible = true;
+            lb_sMin.Visible = true;
+            lb_sMax.Visible = true;
+            lb_vMin.Visible = true;
+            lb_vMax.Visible = true;
+
+            bar_hMin.Visible = true;
+            bar_hMax.Visible = true;
+            bar_sMin.Visible = true;
+            bar_sMax.Visible = true;
+            bar_vMin.Visible = true;
+            bar_vMax.Visible = true;
+
+            lb_hMinValue.Visible = true;
+            lb_hMaxValue.Visible = true;
+            lb_sMinValue.Visible = true;
+            lb_sMaxValue.Visible = true;
+            lb_vMinValue.Visible = true;
+            lb_vMaxValue.Visible = true;
+
+            // set the scroll bars
+            bar_hMin.Value = H_MIN;
+            bar_hMax.Value = H_MAX;
+            bar_sMin.Value = S_MIN;
+            bar_sMax.Value = S_MAX;
+            bar_vMin.Value = V_MIN;
+            bar_vMax.Value = V_MAX;
+
+            bar_Scroll((object)bar_hMin, (EventArgs)null);
+            bar_Scroll((object)bar_hMax, (EventArgs)null);
+            bar_Scroll((object)bar_sMin, (EventArgs)null);
+            bar_Scroll((object)bar_sMax, (EventArgs)null);
+            bar_Scroll((object)bar_vMin, (EventArgs)null);
+            bar_Scroll((object)bar_vMax, (EventArgs)null);
+#endif
+
+            // run the image processing when the operating system is idle
+            Application.Idle += processImageFrisbee;
+        }
+        #endregion
+
+        #region *** Windows Events
+        #region Bar Scroll
+        private void bar_Scroll(object sender, EventArgs e)
+        {
+            TrackBar trackbar = (TrackBar)sender;
+
+            // determine the track bar that changed and update the label with the value
+            switch (trackbar.Name)
+            {
+                case "bar_hMin":
+                    lb_hMinValue.Text = trackbar.Value.ToString();
+                    break;
+
+                case "bar_sMin":
+                    lb_sMinValue.Text = trackbar.Value.ToString();
+                    break;
+
+                case "bar_vMin":
+                    lb_vMinValue.Text = trackbar.Value.ToString();
+                    break;
+
+                case "bar_hMax":
+                    lb_hMaxValue.Text = trackbar.Value.ToString();
+                    break;
+
+                case "bar_sMax":
+                    lb_hMaxValue.Text = trackbar.Value.ToString();
+                    break;
+
+                case "bar_vMax":
+                    lb_vMaxValue.Text = trackbar.Value.ToString();
+                    break;
+            }
+        }
+        #endregion
+
+        #region Image Processing Form Closing
+        private void ImageProcessing_FormClosing(object sender, FormClosingEventArgs e)
+        {
+
+        }
+        #endregion
+        #endregion
+
+        #region Porcess Image Frisbee
+        private void processImageFrisbee(object sender, EventArgs arg)
+        {
+            const double imageWidth = 320;
+            int maxBoundingBoxWidth = 80;
+            int maxBoundingBoxHeight = 10;
+            int maxBoundingBoxArea = 1000;
+            int hMin = H_MIN;
+            int hMax = H_MAX;
+            int sMin = S_MIN;
+            int sMax = S_MAX;
+            int vMin = V_MIN;
+            int vMax = V_MAX;
+            Image<Bgr, Byte> image = capture.QueryFrame();
+            Image<Hsv, byte> hsvImage;
+            Image<Gray, byte> mask;
+
+            try
+            {
+                // Convert the image to hsv
+                hsvImage = image.Convert<Hsv, byte>();
+
+#if USE_SLIDERS
+                // Threshold the HSV Image for targets
+                readTrackBarDelegate readBar = new readTrackBarDelegate(readTrackBar);
+
+                // read the track bar positions
+                hMin = (int)this.Invoke(readBar, bar_hMin);
+                hMax = (int)this.Invoke(readBar, bar_hMax);
+                sMin = (int)this.Invoke(readBar, bar_sMin);
+                sMax = (int)this.Invoke(readBar, bar_sMax);
+                vMin = (int)this.Invoke(readBar, bar_vMin);
+                vMax = (int)this.Invoke(readBar, bar_vMax);
+#endif
+
+                // mask the image for the Frisbees
+                mask = hsvImage.InRange(new Hsv(hMin, sMin, vMin), new Hsv(hMax, sMax, vMax));
+
+                // Filter image
+                mask._Dilate(4);
+                //mask._Erode(4);
+
+                // List of boxes found
+                List<Rectangle> boxList     = new List<Rectangle>();
+                List<Ellipse>   ellipseList = new List<Ellipse>();
+
+
+                for (Contour<Point> contours = mask.FindContours(); contours != null; contours = contours.HNext)
+                {
+                    contours.ApproxPoly(contours.Perimeter * 0.05);
+
+                    Rectangle rect = contours.BoundingRectangle;
+
+                    if (rect.Width  > maxBoundingBoxWidth  &&
+                        rect.Height > maxBoundingBoxHeight &&
+                        contours.Area > maxBoundingBoxArea)
+                    {
+                        boxList.Add(rect);
+                        image.Draw(contours, new Bgr(21, 255, 255), 2);
+                    }
+
+                    // ellipse not used other than for drawing - could be removed
+                    if (contours.Total >= 5) // required for ellipse fitting
+                    {
+                        PointF[] pts = new PointF[contours.Total];
+
+                        for (int i = 0; i < contours.Total; i++)
+                            pts[i] = new PointF(contours[i].X, contours[i].Y);
+
+                        Ellipse ellipse = PointCollection.EllipseLeastSquareFitting(pts);
+
+                        if ((ellipse.MCvBox2D.size.Height > 10) && 
+                            (ellipse.MCvBox2D.size.Width  > 10))
+                            ellipseList.Add(ellipse);
+                    }
+                }
+
+                double minCenter = imageWidth;   // min distance to center of image
+                int centerRectNum = 0;           // number of closest rect
+
+                for (int n = 0; n < boxList.Count; n++)
+                {
+                    Rectangle rec = boxList[n];
+
+                    image.Draw(rec, new Bgr(255, 21, 255), 2);
+
+                    double centerx = rec.X + rec.Width / 2.0;
+
+                    if (Math.Abs(centerx - imageWidth / 2) < minCenter)
+                    {
+                        minCenter = Math.Abs(centerx - imageWidth / 2);
+                        centerRectNum = n;
+                    }
+                }
+
+                for (int n = 0; n < ellipseList.Count; n++)
+                {
+                    image.Draw(ellipseList[n], new Bgr(255, 21, 255), 2);
+                }
+
+                //Console.Write(minCenter);
+                //MessageBox.Show("Pos:"    + (boxList[centerRectNum].X + boxList[centerRectNum].Width/2.0) + "distance: "+ minCenter);
+                //MessageBox.Show("Off by:" + (boxList[centerRectNum].X + boxList[centerRectNum].Width / 2.0 - imageWidth / 2));
+
+                // Print out the information about the first rect
+                if (boxList.Count != 0)
+                {
+                    double offset = (boxList[centerRectNum].X + boxList[centerRectNum].Width / 2.0 - imageWidth / 2);
+                    NetworkTable.getTable("SmartDashboard").putNumber("Frisbee Offset", offset);
+                    NetworkTable.getTable("SmartDashboard").putNumber("Frisbee Size", boxList[centerRectNum].Width);
+                }
+                else
+                {
+                    NetworkTable.getTable("SmartDashboard").putNumber("Frisbee Offset", 0);
+                    NetworkTable.getTable("SmartDashboard").putNumber("Frisbee Size", 320);  //assume it is too big to see
+                }
+
+                im_mask.Image = mask.ToBitmap();
+                im_image.Image = image.ToBitmap();
+            }
+            catch
+            {
+            }
+        }
+        #endregion
+
+        #region Read TrackBar
+        public int readTrackBar(TrackBar bar)
+        {
+            return bar.Value;
+        }
+        #endregion
+
+
+        #region Write Message To TextBox
+        public void writeMessageToTextBox(string message, bool linefeed)
+        {
+            // InvokeRequired required compares the thread ID of the calling thread
+            // to the thread ID of the creating thread. If these threads are
+            // different, it returns true.
+            if (tb_message.InvokeRequired)
+            {
+                WriteMessageDelegate d = new WriteMessageDelegate(writeMessageToTextBox);
+                Invoke(d, new object[] { message, linefeed });
+            }
+            else
+            {
+                // determine if the text box should be cleared
+                if (message == "CLEAR")
+                    tb_message.Text = "";
+                else
+                    tb_message.Text += message;
+
+                // check for adding a carriage return
+                if (linefeed)
+                    tb_message.Text += Environment.NewLine;
+
+                // move the horizontal bar to the end of text
+                tb_message.SelectionStart = tb_message.TextLength;
+                tb_message.ScrollToCaret();
+
+                // ensure the text box is updated
+                this.Update();
+            }
+        }
+        #endregion
+    }
+}
